af33d2a7fe642f8e1b0624662691d8a0928be995
jamesshieh <randomletters13@gmail.com>
2012-11-08 17:55:46 -0500

    added Chingu frontend

--- life.rb
+++ life.rb
@@ -7,60 +7,62 @@ class Environment
 
   def initialize(x=40, y=40)
     @x, @y= x, y
-    @board = self.create_blank_board
-    @next_gen = self.create_blank_board
-    @board[14][16] = 1
-    @board[16][17] = 1
-    @board[15][15] = 1
-    @board[15][16] = 1
-    @board[15][17] = 1
+    @board = create_blank_board
+    @next_gen = create_blank_board
   end
 
   def step
-    @board.each_with_index do |_, i|
-      @board.each_with_index do |_, j|
-        @next_gen[i][j] = 1 if life?(i, j)
+    @board.each_with_index do |row, i|
+      row.each_with_index do |_, j|
+        @next_gen[i][j] = 1 if life?(j, i)
       end
     end
     @board = @next_gen
-    @next_gen = self.create_blank_board
+    @next_gen = create_blank_board
   end
 
   def create_blank_board
     board = []
-    for i in(0..@y-1)
+    for i in(1..@y)
       board << [0] * @x
     end
     return board
   end
 
+  def dead!(x, y)
+    @board[y][x] = 0
+  end
+
+  def alive!(x, y)
+    @board[y][x] = 1
+  end
+
   def alive?(x, y)
-    return @board[x][y] == 1
+    return @board[y][x] == 1
   end
 
   def life?(x, y)
-    if x > 0 and x < @x-1 and y > 0 and y < @y-1
-      neighbors = self.find_neighbors(x,y)
-      if self.alive?(x,y)
-        return (neighbors == 2 or neighbors == 3)
-      else
-        return neighbors == 3
-      end
+    neighbors = find_neighbors(x,y)
+    if @board[y][x] == 1
+      return (neighbors == 2 or neighbors == 3)
     else
-      return false
+      return neighbors == 3
     end
   end
 
   def find_neighbors(x, y)
-    neighbors = 0
-    for i in (x-1..x+1)
-      for j in (y-1..y+1)
-        if !@board[i][j].nil?
-          neighbors += (@board[i][j])
-        end
-      end
-    end
-    neighbors -= @board[x][y]
+    x = x % (@x-1)
+    y = y % (@y-1)
+    neighbors = @board[y-1][x-1] + @board[y-1][x] + @board[y-1][x+1] + @board[y][x-1] + @board[y][x+1] + @board[y+1][x-1] + @board[y+1][x] + @board[y+1][x+1]
+#    neighbors = 0
+#    for i in (x-1..x+1)
+#      for j in (y-1..y+1)
+#        i = i % @x
+#        j = j % @y
+#        neighbors += (@board[j][i])
+#      end
+#    end
+#    neighbors -= @board[y][x]
     return neighbors
   end
 
--- main.rb
+++ main.rb
@@ -1,9 +1,112 @@
+#!/usr/bin/env ruby
+
 require './life'
 require 'chingu'
+include Gosu
+  
+CELL_SIZE = 5
 
-board = Environment.new
-while true
-  board.print_board
-  sleep 0.3
-  board.step
+class Game < Chingu::Window
+  def initialize
+    super(1280, 720, false)
+    $universe = Environment.new($window.width/CELL_SIZE, $window.height/CELL_SIZE)
+    push_game_state(Menu)
+  end
+
 end
+
+class Master < Chingu::GameState
+
+  def initialize
+    super
+  end
+ 
+  def draw_grid
+    $universe.board.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
+        if cell == 1
+          draw_rect(x, y)
+        end
+      end
+    end
+  end
+
+  def draw_rect(x, y)
+    $window.fill_rect([x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE], Color::WHITE, 0)
+  end
+end
+
+# Starting Menu
+
+class Menu < Chingu::GameState
+  def initialize(options = {})
+    super
+    @title = Chingu::Text.create(:text => "Welcome to the Game Of Life!", :x => 300, :y => 300, :size => 30)
+    @prompt = Chingu::Text.create(:text => "Press space to begin.", :x => 800, :y => 300, :size => 30)
+    self.input = { :space => Draw }
+  end
+end
+
+# Free Edit Drawing Mode
+
+class Draw < Master
+  def initialize
+    super
+    @prompt = Chingu::Text.create(:text => "Press Space to simulate", :x => 0, :y => 670, :size => 30)
+    self.input =  {   :escape => :exit,
+                      :space => Sim,
+                      :left_mouse_button => :add_rect,
+                      :right_mouse_button => :remove_rect                    
+                  }
+  end
+
+  def add_rect
+    $universe.alive!($window.mouse_x/CELL_SIZE,$window.mouse_y/CELL_SIZE) 
+  end
+
+  def remove_rect
+    $universe.dead!($window.mouse_x/CELL_SIZE,$window.mouse_y/CELL_SIZE)
+  end
+  
+  def draw_cursor
+    $window.fill_rect([$window.mouse_x, $window.mouse_y, CELL_SIZE, CELL_SIZE], Color::RED, 0)
+  end
+
+  def draw
+    super
+    draw_grid
+    draw_cursor
+  end
+
+  def update
+    super
+  end
+
+end
+
+# Simulation Mode
+
+class Sim < Master
+  def initialize
+    super
+    @prompt = Chingu::Text.create(:text => "Press Space to return to draw mode", :x => 0, :y => 670, :size => 30)
+    self.input = { :escape => :exit, :f1 => :debug, :space => Draw }
+  end
+
+  def debug
+    push_game_state(Chingu::GameStates::Debug.new)
+  end
+
+  def draw
+    super
+    draw_grid
+  end
+  
+  def update
+    super
+    $universe.step
+    $window.caption =  "FPS: #{$window.fps}, Game_Objects: #{$window.game_objects}"
+  end
+end
+
+Game.new.show
